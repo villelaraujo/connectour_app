d4d38dc8f68786a2e79c5076151ad6e60bd46cd2
villelaraujo <arthurvaraujo.aaj@gmail.com>
2026-05-05 19:50:26 -0300

    small header fix, error page init

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -13,8 +13,8 @@
             <div class="hidden md:flex items-center">
                 <button @click="logout" class="cursor-pointer text-sm text-emerald-400 transition-colors duration-150 hover:text-white">Logout</button>
             </div>
-            <button @click="menuOpen=!menuOpen" class="cursor-pointer flex md:hidden items-center p-2">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+            <button @click="menuOpen=!menuOpen" class="cursor-pointer group flex md:hidden items-center p-2">
+                <svg class="group-hover:fill-emerald-300" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
             </button>
         </div>
     </div>
@@ -53,8 +53,5 @@
         height: 32px;
         width: 32px;
         fill: white;
-    }
-    svg:hover{
-        fill: #5ee9b5;
     }
 </style>
