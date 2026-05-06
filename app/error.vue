bbb0a77cb340132756808f3fc454f13c8e359333
villelaraujo <arthurvaraujo.aaj@gmail.com>
2026-05-06 16:32:32 -0300

    global error page

--- app/error.vue
+++ app/error.vue
@@ -1,7 +1,23 @@
 <template>
-    
+    <SectionWrapper id="error">
+        <div class="w-screen overflow-hidden h-screen">
+            <div class="flex items-center justify-center">
+                <h1 class="text-5xl">An Error Occurred</h1>
+                <button @click="handleError" class="text-sm text-emerald-400 transition-colors duration-150 hover:text-white"></button>
+            </div>
+        </div>
+    </SectionWrapper>
 </template>
 
 <script setup>
+    definePageMeta({
+        layout: 'pre-auth',
+    })
+    useHead({
+        title: 'Error'
+    })
 
+    function handleError(){
+        clearError({redirect: '/login'});
+    }
 </script>
