281b8690c493586b1e5c9b6c8eb7fa0f8c11b3e7
villelaraujo <arthurvaraujo.aaj@gmail.com>
2026-06-01 18:15:54 -0300

    wip6: BetterAuth completed session getter wrapper at index page

---
+++ app/composables/useAuthSession.ts
@@ -0,0 +1,11 @@
+import { authClient } from "~/lib/authClient";
+
+export default function useAuthSession(url:string, options?: any){
+    const client = authClient;
+    try {
+        if(url.startsWith('http')) url = new URL(url).pathname;
+    } catch (error) {
+        console.error('Error parsing URL:', error);
+    }
+    return useFetch(url, options) as any;
+}
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,7 @@
                 <h2 class="text-lg text-neutral-300">Write a Log</h2>
             </div>
             <div v-else class="flex items-center justify-between border p-2 sm:p-4 border-neutral-700">
-                <h2 class="text-md md:text-xl font-semibold pl-2 sm:pl-4">Welcome,<span class="pl-2 text-neutral-400">{{userdata?.user?.name || "none"}}</span></h2>
+                <h2 class="text-md md:text-xl font-semibold pl-2 sm:pl-4">Welcome,<span class="pl-2 text-neutral-400">{{session?.user?.name || "none"}}</span></h2>
                 <button v-if="$route.query.logbook" @click="toggleCreateMode" class="cursor-pointer flex items-center gap-2 py-2 px-4 bg-neutral-800 border border-neutral-700 shadow-md text-sm sm:text-lg transition-colors duration-150 hover:border-emerald-600 hover:bg-emerald-800">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z"/></svg>
                     <span>Create Log</span>
@@ -49,9 +49,7 @@
     });
     definePageMeta({middleware:''});
     
-    const {data:session} = await authClient.useSession();
-
-    const {data: userdata} = await useLazyFetch('/api/userdata');
+    const {data:session} = await authClient.useSession(useAuthSession);
 
     const route = useRoute();
 
